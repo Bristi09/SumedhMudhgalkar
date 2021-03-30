1d29034f6ebdc51a55f65353d56bf833a6d75dc4
Bristi09 <77116606+Bristi09@users.noreply.github.com>
2021-03-30 09:24:04 +0600

    Add files via upload

---
+++ jQuary/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.sumidh-text-box').animatedHeadline({
+        animationType: 'clip'
+    });
+})
+
+
+//
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 300,
+  wrap: false
+})
